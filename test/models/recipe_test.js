17b2bec76b6e8928d02b97e4bea1e4389844f855
colleenmcguckin <colleenmcguckin@gmail.com>
2015-10-09 11:40:55 -0700

    installed jasmine

---
+++ test/models/recipe_test.js
@@ -0,0 +1,13 @@
+require 'jasmine'
+
+# class RecipeTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
+
+describe("search function", function() {
+  it("shows what you searched for", function() {
+    expect().toEqual("Hello world!");
+  });
+});
